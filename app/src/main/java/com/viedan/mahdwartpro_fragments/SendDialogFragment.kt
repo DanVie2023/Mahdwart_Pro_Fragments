7703bb8546fa01883e73c0e12f1a587c8fdb41c7
DanVie2023 <Daniel.Vieler@web.de>
2026-06-15 16:54:21 +0200

    E-Mail connection in SendDialogFragment.kt Titlebar in fragment_mahdwart.xml

--- app/src/main/java/com/viedan/mahdwartpro_fragments/SendDialogFragment.kt
+++ app/src/main/java/com/viedan/mahdwartpro_fragments/SendDialogFragment.kt
@@ -1,10 +1,15 @@
 package com.viedan.mahdwartpro_fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.makeText
 import com.viedan.mahdwartpro_fragments.databinding.FragmentSendDialogBinding
 
 class SendDialogFragment : Fragment() {
@@ -16,6 +21,8 @@ class SendDialogFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         wtgs = arguments?.getStringArrayList("wtgs") ?: emptyList()
+
+
     }
 
     override fun onCreateView(
@@ -36,6 +43,30 @@ class SendDialogFragment : Fragment() {
 
         binding.buttonClose.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+
+        binding.buttonSend.setOnClickListener {
+            val recipient = binding.listEmail.text.toString()
+            val subject = binding.InputTextEmailSubject.text.toString()
+            val message = binding.InputTextEmailBody.text.toString()
+
+            val mIntent = Intent(Intent.ACTION_SEND)
+
+            mIntent.data = Uri.parse("mailto:")
+            mIntent.type = "text/plain"
+            mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+            mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            mIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+            try {
+                startActivity(Intent.createChooser(mIntent,"Send Email"))
+            } catch (e: Exception){
+                // Toast.makeText(
+                 //   requireContext(),
+                 //   e.message,
+                 //   LENGTH_LONG
+                //).show()
+            }
         }
     }
 }
